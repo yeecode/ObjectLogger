83853dd2d6a850872539fb13636b2a185e7f1222
Barry <diancitie@126.com>
2019-04-08 22:02:15 +0800

    init

---
+++ src/main/java/com/github/yeecode/objectLogger/server/ObjectLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.github.yeecode.objectLogger.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectLoggerApplication.class, args);
+	}
+
+}
