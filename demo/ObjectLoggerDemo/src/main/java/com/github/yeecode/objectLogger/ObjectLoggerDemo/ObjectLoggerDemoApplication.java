0515c36044486a9864942fd3d60c17dfd3aa4a39
yee <diancitie@hotmail.com>
2019-06-27 21:24:43 +0800

    add demo folder

---
+++ demo/ObjectLoggerDemo/src/main/java/com/github/yeecode/objectLogger/ObjectLoggerDemo/ObjectLoggerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.yeecode.objectLogger.ObjectLoggerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectLoggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectLoggerDemoApplication.class, args);
+	}
+
+}
