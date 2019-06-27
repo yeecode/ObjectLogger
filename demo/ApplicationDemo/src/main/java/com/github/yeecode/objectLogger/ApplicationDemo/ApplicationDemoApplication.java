8005cf5bac0689ef7eea7b798c96da135e828ad2
yee <diancitie@hotmail.com>
2019-06-27 21:45:17 +0800

    add demo files

---
+++ demo/ApplicationDemo/src/main/java/com/github/yeecode/objectLogger/ApplicationDemo/ApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.yeecode.objectLogger.ApplicationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationDemoApplication.class, args);
+	}
+
+}
