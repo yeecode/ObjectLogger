fda5257ba3b7b98e72b43a3ae120c775f07a6352
yee <diancitie@hotmail.com>
2019-07-02 10:21:22 +0800

    2.0.1:Replace HTTP request jar package; protect multi thread protection for HTTP requests

--- client/src/main/java/com/github/yeecode/objectLogger/client/http/HttpBean.java
+++ client/src/main/java/com/github/yeecode/objectLogger/client/http/HttpBean.java
@@ -1,20 +1,17 @@
 package com.github.yeecode.objectLogger.client.http;
 
 
-import com.alibaba.fastjson.JSON;
 import com.github.yeecode.objectLogger.client.config.ObjectLoggerConfigBean;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,61 +22,60 @@ public class HttpBean {
     @Autowired(required = false)
     private ObjectLoggerConfigBean objectLoggerConfigBean;
 
-    public void sendLog(String jsonString) {
-        try{
-            Map<String,String> logParamMap = new HashMap<>();
-            logParamMap.put("logJsonString", URLEncoder.encode(jsonString, "utf-8"));
-            String response = jsonPost(objectLoggerConfigBean.getAddLogApi(),logParamMap);
+    public synchronized void sendLog(String jsonString) {
+        try {
+            Map<String, Object> logParamMap = new HashMap<>();
+            logParamMap.put("logJsonString", jsonString);
+            sendPost(objectLoggerConfigBean.getAddLogApi(), logParamMap, "utf-8");
         } catch (Exception ex) {
             LOGGER.error("sendLog error!", ex);
         }
     }
 
 
-    private String jsonPost(String strURL, Map<String, String> params) {
-        try {
-            URL url = new URL(strURL);// 创建连接
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setUseCaches(false);
-            connection.setInstanceFollowRedirects(true);
-            connection.setRequestMethod("POST"); // 设置请求方式
-            connection.setRequestProperty("Accept", "application/json"); // 设置接收数据的格式
-            connection.setRequestProperty("Content-builtinType", "application/json"); // 设置发送数据的格式
-            connection.connect();
-            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8"); // utf-8编码
-            out.append(JSON.toJSONString(params));
-            out.flush();
-            out.close();
-
-            int code = connection.getResponseCode();
-            InputStream is = null;
-            if (code == 200) {
-                is = connection.getInputStream();
-            } else {
-                is = connection.getErrorStream();
+    private synchronized String sendPost(String urlParam, Map<String, Object> params, String charset) {
+        StringBuffer resultBuffer = null;
+        // 构建请求参数
+        StringBuffer sbParams = new StringBuffer();
+        if (params != null && params.size() > 0) {
+            for (Map.Entry<String, Object> e : params.entrySet()) {
+                sbParams.append(e.getKey());
+                sbParams.append("=");
+                sbParams.append(e.getValue());
+                sbParams.append("&");
             }
-
-            // 读取响应
-            int length = (int) connection.getContentLength();// 获取长度
-            if (length != -1) {
-                byte[] data = new byte[length];
-                byte[] temp = new byte[512];
-                int readLen = 0;
-                int destPos = 0;
-                while ((readLen = is.read(temp)) > 0) {
-                    System.arraycopy(temp, 0, data, destPos, readLen);
-                    destPos += readLen;
-                }
-                String result = new String(data, "UTF-8"); // utf-8编码
-                return result;
-            }
-
-        } catch (IOException e) {
-            LOGGER.error("Exception occur when send http post request!", e);
         }
-        return "error"; // 自定义错误信息
-    }
+        HttpURLConnection con = null;
+        // 发送请求
+        try {
+            URL url = new URL(urlParam);
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setUseCaches(false);
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            if (sbParams.length() > 0) {
+                try (OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream(), charset)) {
+                    osw.write(sbParams.substring(0, sbParams.length() - 1));
+                    osw.flush();
+                }
+            }
+            // 读取返回内容
+            resultBuffer = new StringBuffer();
+            int contentLength = Integer.parseInt(con.getHeaderField("Content-Length"));
+            if (contentLength > 0) {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), charset))) {
+                    String temp;
+                    while ((temp = br.readLine()) != null) {
+                        resultBuffer.append(temp);
+                    }
+                }
 
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return resultBuffer.toString();
+    }
 }
